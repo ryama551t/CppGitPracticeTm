4dce42a8a4609c309e93c9370b07e306b444a7c4
ryama551t <150801837+ryama551t@users.noreply.github.com>
2024-01-18 14:58:44 +0900

    演習のため、関数実装を削除

--- FindFibonacciGcd.cpp
+++ FindFibonacciGcd.cpp
@@ -1,23 +1,16 @@
 #include "pch.h"
 #include "FindFibonacciGcd.h"
 
+/* ‚±‚±‚ÉFindFibonacciGcd.h‚Å’è‹`‚µ‚½ŠÖ”‚ğÀ‘•‚·‚é@*/
+
 uint32_t gcd(uint32_t a, uint32_t b) {
-	if (a == 0)
-		return b;
-	return gcd(b % a, a);
+	return 0;
 }
+
 uint32_t fibonacci(uint32_t n) {
-	if (n <= 0)
-		return 0;
-	if (n == 1)
-		return 1;
-	return fibonacci(n - 1) + fibonacci(n - 2);
+	return 0;
 }
-uint32_t findFibonacciGcd(uint32_t n, uint32_t m) {
-	uint32_t x = gcd(fibonacci(n), fibonacci(m));
-	for (uint32_t i = 0; i < (2 << 16); i++){
-		if(x == fibonacci(i))
-			return i;
-	}
-	return -1;
+
+uint32_t findFibonacciGcd(uint32_t a, uint32_t b) {
+	return 0;
 }
